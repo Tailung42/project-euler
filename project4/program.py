8757a2771bf3b4dc4046975673bc2ef05417404b
Tai Lung <tailung12345678910@gmail.com>
2024-12-30 06:27:40 +0000

    mark2: more efficient

--- project4/program.py
+++ project4/program.py
@@ -2,19 +2,23 @@ lower_limit = 100
 upper_limit = 999
 
 def main():
-    greatest_palindrome = 0
+    greatest_palindrome, num1_result, num2_result = 0, 0, 0
     for num1 in range(upper_limit, lower_limit-1, -1):
         for num2 in range(num1, lower_limit-1, -1 ):
             product = num1 * num2
-            if is_palindrome(product) and product > greatest_palindrome:
+            if product < greatest_palindrome:
+                break # products will get smaller
+            if is_palindrome(product):
                 greatest_palindrome = product
-                break  # num1 * (num2 - any number ) will always yield smaller result
-            
-        # if palindrome is found and we've checked all upper half no need to check lower half
-        if num1 < upper_limit / 2 and greatest_palindrome:
-            break
+                num1_result,num2_result = num1, num2
+                break # no need to continue for smaller num2
+
+        # Once a palindrome is found, products with smaller num1 values will not exceed it
+        # if num1 < upper_limit / 2 and greatest_palindrome:
+        #     break
+
     if greatest_palindrome:
-        print(f"Greatest palindrome product: {greatest_palindrome}")
+        print(f"Greatest palindrome product {num1_result} * {num2_result} = {greatest_palindrome}")
     else:
         print("there is no palindrome within the range")
 
